25ff367b5338e3344b5526ba67e314ff20c59b91
Chunsu Lee <chunsu1002@gmail.com>
2017-11-02 08:09:47 +0900

    ちょい修正

--- fuel/app/views/top/index.php
+++ fuel/app/views/top/index.php
@@ -6,11 +6,34 @@
     <?php echo Asset::img('scratch.jpg',array()) ?>
 </div>
 <div class="row">
+    <h4>NKラボについて</h4>
+    <p>
+        AI,IOT,ドローン,仮想通貨などのテクノロジーの発展により、社会が大きく変わろうとしています。<br>
+        2017年現時点では、まだ上記のテクノロジーは一部もしくは実験的に導入されているとはいえ、<br>
+        まだまだ発展途上であり、根本的にそれらを開発もしくはコントロールできる体制が整っておらず、<br>
+        ましてやそれらに携わる人材は完全に不足しております。<br>
+        NKラボは上記テクノロジーが実際に社会に普及し、世の中の生産性が飛躍的に高まる時代に<br>
+        その社会を牽引出来る人材育成、誰もがプロダクトの生産を行えるプラットホームとなるべく<br>
+        結成されました。
+    </p>
+    <p>
+        目指すところは、探査機開発です。
+    </p>
+</div>
+<div class="row">
+    <h4>NKキッズラボについて</h4>
+    <p>
+        知識経済、テクノロジーの発展とともにエンジニアの数が少ないため、<br>
+        日本の小学校では、2020年に小学生からプログラミングを必修とされる予定です。<br>
+        NKキッズラボでは、特にウリハッキョの子供達に、日本学校よりも早めに覚えてもらうためにプログラミ
+    </p>
+</div>
+<div class="row">
     <div class="__point">
+        <!--<div class="col-md-12">-->
         <div class="col-md-4">
-        <div class="col-md-12">
             <h3>
-                <span class="__tp">Point <span>1</span></span>
+                <span class="__tp">身につくこと <span>1</span></span>
                 <span class="__text"><em>論理的思考</em>が身に付く</span>
             </h3>
             <p>
@@ -21,9 +44,9 @@
                 プログラミングを通して、論理的思考を伸ばすことにつながります。
            　</p>
         </div>
-        <div class="col-md-12">
+        <div class="col-md-4">
             <h3>
-                <span class="__tp">Point <span>2</span></span>
+                <span class="__tp">身につくこと <span>2</span></span>
                 <span class="__text"><em>リーダーシップ</em>を育む</span>
             </h3>
             <p>
@@ -33,9 +56,9 @@
                 コミュニケーション能力やリーダーシップを伸ばすことが出来ます
             </p>
         </div>
-        <div class="col-md-12">
+        <div class="col-md-4">
             <h3>
-                <span class="__tp">Point <span>3</span></span>
+                <span class="__tp">身につくこと <span>3</span></span>
                 <span class="__text"><em>自主性、創造性</em>を育む</span>
             </h3>
             <p>
@@ -45,7 +68,7 @@
                 ものづくりを体験し、世の中にプロダクトを出す精神、チャレンジ精神を育みます。
             </p>
         </div>
-        </div>
+        <!--</div>-->
     </div>
 </div>
 <div class="row">
