2768b408bb4ea0a6d5249f98eb2fd9915ea5bbe0
Chunsu Lee <chunsu1002@gmail.com>
2017-11-03 10:32:14 +0900

    でバック

--- public/assets/js/top/top.js
+++ public/assets/js/top/top.js
@@ -1,15 +1,16 @@
 $(function() {
+    $(".mid_price").text(bitblyer_controller);
     //1秒ごとの最新を取りに行く
-    setInterval(function(){
-        //価格get
-        $.ajax({
-            url:bitblyer_controller,
-            type:'GET',
-            dataType : "json",
-            success : function(data){
-                console.log(data.mid_price);
-                $(".mid_price").text(data.mid_price);
-            }
-        });
-    },1000);
+//    setInterval(function(){
+//        //価格get
+//        $.ajax({
+//            url:bitblyer_controller,
+//            type:'GET',
+//            dataType : "json",
+//            success : function(data){
+//                console.log(data.mid_price);
+//                $(".mid_price").text(data.mid_price);
+//            }
+//        });
+//    },1000);
 });
